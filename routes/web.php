1ec413314ed1a63cda9d8d51761a25af49b5c821
Manuel <mayala@inmovilla.com>
2024-05-18 00:22:47 +0200

    Fuerza deploy

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'welcome';
-});
-
-
-
-Route::get('/prueba', function () {
-    return 'welcome prueba';
+    return 'welcomes';
 });
